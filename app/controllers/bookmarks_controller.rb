a6821456885c9c2bbab9ab320bbd425111fd9cc5
Grazia Cabras <graziacabras.gcabras@gmail.com>
2022-05-26 18:17:11 +0200

    Add the styling

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,34 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.create(bookmarks_params)
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(bookmarks_params[:movie_id])
+    @bookmark.list = @list
+    @bookmark.movie = @movie
+    @bookmark.save
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list_id = @bookmark.list_id
+    @list = List.find(@list_id)
+    @bookmark.destroy
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def bookmarks_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create] do
+    end
+  end
+
+  delete 'bookmarks/:id', to: 'bookmarks#destroy', as: :bookmark_destroy
 end
